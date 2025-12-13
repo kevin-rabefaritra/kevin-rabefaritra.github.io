9ecfa047448f2975b0c12ea6359e41f08e05bc82
Kevin Michel <kevin.rabefaritra@gmail.com>
2025-12-13 13:27:03 +0100

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,9 @@ import { PharmadaComponent } from './projects/pharmada/pharmada.component';
 import { PrivacyPolicyComponent } from './projects/pharmada/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-
-  { path: 'home', title: 'site de kevin', component: HomeComponent },
+  { path: '', title: 'site de kevin', component: HomeComponent },
 
   // Projects
   { path: 'pharmada', title: 'Pharmada', component: PharmadaComponent },
   { path: 'pharmada/privacy-policy', title: 'Pharmada - Privacy policy', component: PrivacyPolicyComponent },
-
 ];
